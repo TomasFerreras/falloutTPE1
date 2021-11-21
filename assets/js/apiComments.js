1ef09dbe680db736b1805da5f4ac0ec8a49bf038
Tomas Ferreras <tomasferrerasexa@gmail.com>
2021-11-21 18:42:58 -0300

    everithing but working :)

--- assets/js/apiComments.js
+++ assets/js/apiComments.js
@@ -2,46 +2,41 @@
 let title = document.querySelector("#item_tittle");
 let id_item = title.dataset.item;
 
-let comment_id = document.querySelector("#comment.id");
-console.log (comment_id);
-
 const API_URL = `api/Item`
 
-console.log(API_URL);
 
+document.querySelector("#deleteBtn").addEventListener('click', deleteComment);
 
 
 let app = new Vue({
     el: "#comment-list",
     data: {
         comments: []
+    },
+    methods: {
+        deleteComment: deleteComment
     }
 })
 
 async function getComments(){
     try {
-        console.log("pijka");
-        let response = await fetch(`API_URL/${id_item}`);
+        let response = await fetch(API_URL + "/" + id_item);
         let comments = await response.json();
         app.comments = comments;
     } catch (e) {
-      
         console.log(e);
     }
 }
 
-// async function Delete(){
-//     try {
-//         let res2 = await fetch(url2); //GET url
-//         let json2 = await res2.json();//texto json a objeto
-//              await fetch(`${url2}/${editions.id}`, {
-//                     "method": "DELETE"
-//                 });
-          
-        
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+async function deleteComment(comment_id){
+    try {
+        await fetch(`${API_URL}/${comment_id}`, {
+            "method": "DELETE"
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    getComments()
+}
 
 getComments();
--- templates/vue/commentsVue.tpl
+++ templates/vue/commentsVue.tpl
@@ -2,7 +2,7 @@
     <div id = "comment-list">
             <ul>
                 <li v-for="comment in comments" class="comments">
-                    User:{{comment.id}} Comment: {{comment.comentario}} Rating: {{comment.valoracion}} <button :id="comment.id"> X </button>
+                    User:{{comment.id}} Comment: {{comment.comentario}} Rating: {{comment.valoracion}} <button id="deleteBtn" v-on:click="deleteComment(comment.id)"> X </button>
                 </li>
             <ul>
     <div>
